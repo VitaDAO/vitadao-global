427bbc17139f6177ed2b5d0f62dca4cfcc91c0b9
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-08-29 10:28:35 +0300

    Remove design-system, update privacy-policy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,15 +1,17 @@
 export default function Page() {
   return (
-    <div className="mx-auto max-w-5xl space-y-5 p-4 @container">
-      <h1 className="text-h2 font-semibold">Privacy policy</h1>
-      <p className="text-xs sm:text-base">
-        Elit Lorem amet et id labore duis tempor reprehenderit do. Velit id non
-        dolor amet enim dolore. Quis adipisicing quis consequat nostrud
-        voluptate anim cillum. In proident irure nostrud pariatur Lorem sit.
-        Aliqua elit ipsum tempor. Id anim voluptate aliqua dolor sit ad dolor
-        sit eu excepteur veniam aute ex. Dolore ex consectetur laboris est
-        eiusmod et eu nisi.
-      </p>
+    <div className="mx-auto max-w-[1260px] @container">
+      <div className="px-[20px] py-[30px] @xl:px-[30px] @xl:pt-[90px]">
+        <h1 className="mb-[10px] text-h2 font-medium">Privacy policy</h1>
+        <p className="mb-[30px] max-w-[770px] text-base">
+          Elit Lorem amet et id labore duis tempor reprehenderit do. Velit id
+          non dolor amet enim dolore. Quis adipisicing quis consequat nostrud
+          voluptate anim cillum. In proident irure nostrud pariatur Lorem sit.
+          Aliqua elit ipsum tempor. Id anim voluptate aliqua dolor sit ad dolor
+          sit eu excepteur veniam aute ex. Dolore ex consectetur laboris est
+          eiusmod et eu nisi.
+        </p>
+      </div>
     </div>
   );
 }
