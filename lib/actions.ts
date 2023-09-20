7b520934a19f9a6148601842ab0958287835958a
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-09-20 17:08:55 +0300

    Attemp to improve user deletion flow

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,10 +1,13 @@
 "use server";
 
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { deleteAuthenticatedUser } from "@/lib/users";
 
 export async function deleteUser() {
   await deleteAuthenticatedUser();
+  cookies().delete("privy-token");
+  cookies().delete("privy-refresh-token");
   redirect("/");
 }
