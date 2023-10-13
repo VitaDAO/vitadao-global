07e9fc198e86e3e9e41d5d0211c1e24d50f13fda
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-10-14 01:23:57 +0300

    Add actual Hotjar snippet

--- lib/tracking/scripts.tsx
+++ lib/tracking/scripts.tsx
@@ -23,7 +23,7 @@ interface TrackingScriptProps {
 export function Hotjar({ env }: TrackingScriptProps) {
   let scriptContent = `console.log("Loaded Hotjar development mock.")`;
   if (env === "production") {
-    scriptContent = `console.log("Loaded Hotjar production mock.")`;
+    scriptContent = `(function(h,o,t,j,a,r){h.hj=h.hjfunction(){(h.hj.q=h.hj.q[]).push(arguments)};h._hjSettings={hjid:3688552,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
   }
 
   return (
