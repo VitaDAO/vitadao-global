1a0bd5dd04ff8b827bf0ba9442c99072c0625da4
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-10-21 17:58:35 +0300

    Improve consent-dialog layout

--- lib/tracking/consent-dialog.tsx
+++ lib/tracking/consent-dialog.tsx
@@ -16,7 +16,7 @@ function UnwrappedConsentDialog() {
     <Dialog.Root open={open} onOpenChange={setOpen} modal={false}>
       <Dialog.Portal>
         <Dialog.Content
-          className="fixed bottom-0 left-[50%] z-50 w-full max-w-md translate-x-[-50%] space-y-[10px] rounded-t-3xl bg-background p-[20px] shadow-[rgba(55,65,81,0.15)_0px_8px_36px] duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-bottom-[48%] data-[state=open]:slide-in-from-left-1/2 sm:bottom-[20px] sm:rounded-3xl"
+          className="fixed bottom-0 left-[50%] z-50 w-full translate-x-[-50%] space-y-[10px] rounded-t-3xl bg-background p-[20px] shadow-[rgba(55,65,81,0.15)_0px_8px_36px] duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-bottom-[48%] data-[state=open]:slide-in-from-left-1/2 sm:bottom-[20px] sm:max-w-md sm:rounded-3xl"
           onEscapeKeyDown={(e) => {
             e.preventDefault();
           }}
