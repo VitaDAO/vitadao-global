ecd4380329e8c136a59e510c3cca49993f070b10
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-10-24 22:07:23 +0300

    Restyle dialogs on narrow viewports

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -41,7 +41,10 @@ const AlertDialogContent = React.forwardRef<
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-3xl bg-background p-6 shadow-[rgba(55,65,81,0.15)_0px_8px_36px] duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] md:w-full",
+        "fixed bottom-0 left-1/2 z-50 grid w-full -translate-x-1/2 gap-4 rounded-t-3xl bg-background p-6 shadow-[rgba(55,65,81,0.15)_0px_8px_36px] sm:bottom-auto sm:top-1/2 sm:max-w-lg sm:-translate-y-1/2 sm:rounded-3xl",
+        "duration-200",
+        "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-bottom-[48%] data-[state=open]:slide-in-from-left-1/2 sm:data-[state=open]:slide-in-from-top-[48%]",
+        "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=closed]:slide-out-to-left-1/2 sm:data-[state=closed]:slide-out-to-top-[48%]",
         className,
       )}
       {...props}
