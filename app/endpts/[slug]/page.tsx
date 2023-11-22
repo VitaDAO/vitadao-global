178704799199efe7ac0e945b7810dee5a85b3a72
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-11-22 19:01:17 +0100

    Improve error handling for endpts routes

--- app/endpts/[slug]/page.tsx
+++ app/endpts/[slug]/page.tsx
@@ -1,37 +1,46 @@
+import { ErrorUi } from "@/components/ui/error-ui";
 import { getArticle } from "@/lib/services/endpts";
 
 export const revalidate = 300;
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const { title, body, imageSrc } = await getArticle(params.slug);
+  try {
+    const { title, body, imageSrc } = await getArticle(params.slug);
 
-  return (
-    <>
-      {imageSrc && (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img
-          src={imageSrc}
-          width={1140}
-          height={480}
-          alt=""
-          className="mb-[10px] aspect-[1140/480] w-full rounded-[20px] object-cover @3xl/main:mb-[30px]"
-        />
-      )}
-      <div className="max-w-[65ch]">
-        <h1 className="mb-[30px] mt-[20px] text-lg/[26.4px] font-medium tracking-[-0.24px] @3xl/main:hidden">
-          {title}
-        </h1>
-        <h1 className="mb-[30px] hidden text-h2 font-medium @3xl/main:block">
-          {title}
-        </h1>
-      </div>
-      {body && (
-        <div
-          // TODO please don't forget to somehow sanitize this
-          dangerouslySetInnerHTML={{ __html: body }}
-          className="prose text-black"
-        />
-      )}
-    </>
-  );
+    return (
+      <>
+        {imageSrc && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={imageSrc}
+            width={1140}
+            height={480}
+            alt=""
+            className="mb-[10px] aspect-[1140/480] w-full rounded-[20px] object-cover @3xl/main:mb-[30px]"
+          />
+        )}
+        <div className="max-w-[65ch]">
+          <h1 className="mb-[30px] mt-[20px] text-lg/[26.4px] font-medium tracking-[-0.24px] @3xl/main:hidden">
+            {title}
+          </h1>
+          <h1 className="mb-[30px] hidden text-h2 font-medium @3xl/main:block">
+            {title}
+          </h1>
+        </div>
+        {body && (
+          <div
+            // TODO please don't forget to somehow sanitize this
+            dangerouslySetInnerHTML={{ __html: body }}
+            className="prose text-black"
+          />
+        )}
+      </>
+    );
+  } catch (e) {
+    if (e instanceof Error) {
+      return <ErrorUi error={e} />;
+    } else {
+      return <ErrorUi error={new Error("Unknown error")} />;
+    }
+  }
 }
--- components/ui/error-ui.tsx
+++ components/ui/error-ui.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { AuthButtons } from "@/components/ui/auth-buttons";
+import { NotAuthenticatedError, NotEnoughVitaError } from "@/lib/errors";
 import vitaPlanet from "@/public/vita-planet.png";
 
 interface ErrorUiProps {
@@ -8,6 +10,53 @@ interface ErrorUiProps {
 }
 
 export function ErrorUi({ error }: ErrorUiProps) {
+  let content;
+
+  if (error instanceof NotAuthenticatedError) {
+    content = (
+      <>
+        <p className="mb-[20px] text-h4 font-medium">
+          You need to log in to view this page.
+        </p>
+        <div className="mb-[20px] flex w-full max-w-sm flex-col gap-5">
+          <AuthButtons />
+        </div>
+      </>
+    );
+  } else if (error instanceof NotEnoughVitaError) {
+    content = (
+      <>
+        <p className="mb-[20px] text-h4 font-medium">
+          You need at least {error.minVita} VITA to view this page.
+        </p>
+        <div className="mb-[20px] max-w-sm space-y-3">
+          <p className="text-center">
+            We recommend using CoW Swap or Uniswap to buy VITA for ease of use
+            and security.
+          </p>
+          <div className="flex flex-col gap-5">
+            <a
+              href="https://cowswap.exchange/#/swap?referral=0xF5307a74d1550739ef81c6488DC5C7a6a53e5Ac2&inputCurrency=ETH&outputCurrency=0x81f8f0bb1cb2a06649e51913a151f0e7ef6fa321&chain=mainnet"
+              target="_blank"
+              className="inline-flex h-[52px] w-full items-center justify-center rounded-full bg-[#042B64] px-3 py-2 text-center text-[#CAE9FE]"
+            >
+              <span className="icon--vita icon--vita--cow-swap mr-2 text-lg" />
+              CoW Swap
+            </a>
+            <a
+              href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x81f8f0bb1cb2a06649e51913a151f0e7ef6fa321&chain=mainnet"
+              target="_blank"
+              className="inline-flex h-[52px] w-full items-center justify-center rounded-full bg-[#FF007A] px-3 py-2 text-center text-white"
+            >
+              <span className="icon--vita icon--vita--uniswap mr-2 text-lg" />
+              Uniswap
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-center p-[20px] pt-[90px]">
       <Image
@@ -17,8 +66,7 @@ export function ErrorUi({ error }: ErrorUiProps) {
         width={668}
         className="mb-[20px]"
       />
-      <h2 className="text-h4 font-medium">{error.name}</h2>
-      <p className="mb-[20px]">{error.message}</p>
+      {content}
       <Link
         href="/"
         className="font-semibold text-vita-purple underline underline-offset-4"
