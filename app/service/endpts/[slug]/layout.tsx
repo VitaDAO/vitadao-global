222d28ceb2c9a48113df75b99cb3c1f44ed7fbb7
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-12-05 18:03:59 +0100

    Center spinner for service/endpts

--- app/service/endpts/[slug]/layout.tsx
+++ app/service/endpts/[slug]/layout.tsx
@@ -1,12 +1,11 @@
-
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="px-0 pt-[30px] sm:px-[30px] sm:pb-[30px] sm:pt-[90px]">
-      <div className="rounded-[20px] bg-white p-[20px] pb-[30px] @xl/main:p-[30px]">
+    <div className="flex flex-grow flex-col px-0 pt-[30px] sm:px-[30px] sm:pb-[30px] sm:pt-[90px]">
+      <div className="flex flex-grow flex-col rounded-[20px] bg-white p-[20px] pb-[30px] @xl/main:p-[30px]">
         {children}
       </div>
     </div>
