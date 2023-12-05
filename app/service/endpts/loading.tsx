691b9654c40d9fffb6f4e69e33903d66c79b2280
Raúl R. Pearson <raulrpearson@protonmail.com>
2023-12-05 17:07:28 +0100

    Center main loading spinner

--- app/service/endpts/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { VitadaoSpinner } from "@/components/ui/vitadao-spinner";
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center py-[30px]">
-      <VitadaoSpinner />
-    </div>
-  );
-}
